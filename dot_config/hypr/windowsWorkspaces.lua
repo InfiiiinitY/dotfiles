45dbc30e0b6b0f85c8eff927323e4db40c35ccef
InfiiiinitY <jamy0498@googlemail.com>
2026-09-09 21:07:00 +0200

    chore(hyprland): formatting fixes

--- dot_config/hypr/windowsWorkspaces.lua
+++ dot_config/hypr/windowsWorkspaces.lua
@@ -1,47 +1,59 @@
 -- Ignore maximize requests from apps. You'll probably like this.
 hl.window_rule({
-    name = "suppress-maximize-events",
-    match = {
-        class = ".*",
-    },
-    suppress_event = "maximize"
+	name = "suppress-maximize-events",
+	match = {
+		class = ".*",
+	},
+	suppress_event = "maximize",
 })
 
 -- Add red border to pinned windows
 hl.window_rule({
-    match = {
-        pin = true,
-    },
-    border_color = {
-        colors = { "rgb(FF0000)", "rgb(880808)" }, angle = 45
-    }
+	match = {
+		pin = true,
+	},
+	border_color = {
+		colors = { "rgb(FF0000)", "rgb(880808)" },
+		angle = 45,
+	},
 })
 
 -- No border on special workspace windows
 hl.workspace_rule({
-    workspace = "special:magic",
-    no_border = true,
+	workspace = "special:magic",
+	no_border = true,
 })
 
 -- Add blur to waybar
 hl.layer_rule({
-    match = {
-        namespace = "waybar",
-    },
-    blur = true,
+	match = {
+		namespace = "waybar",
+	},
+	blur = true,
 })
 -- Don't blur pixels that have an alpha of 0 (fully transparent)
 hl.layer_rule({
-    match = {
-        namespace = "waybar",
-    },
-    ignore_alpha = 0,
+	match = {
+		namespace = "waybar",
+	},
+	ignore_alpha = 0,
+})
+
+hl.layer_rule({
+	name = "noctalia",
+	match = {
+		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd|window-switcher)$",
+	},
+	no_anim = true,
+	ignore_alpha = 0.5,
+	blur = true,
+	blur_popups = true,
 })
 
 -- Disable animations on selection
 hl.layer_rule({
-    match = {
-        namespace = "selection",
-    },
-    no_anim = true,
+	match = {
+		namespace = "selection",
+	},
+	no_anim = true,
 })
